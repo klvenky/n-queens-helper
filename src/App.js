53acf1030dfe933c34bccf157244757604dd7880
klvenky <venkatesh@convertcart.com>
2019-02-18 16:21:19 +0530

    fixed reverse diagonal logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo.svg';
+import isEqual from 'lodash/isEqual';
+import _find from 'lodash/find';
+import { findDisabledPos, getBoxStyle } from './utils';
+
+const DEFAULT = 8;
+const ARR = [];
+for (let i = 0; i < DEFAULT; i += 1) ARR[i] = i;
 
 function App() {
+  const [queens] = useState(DEFAULT);
+  const [queenBlocks, setQueenBlocks] = useState([]);
+  const [disabledPositions, setDisabledPositions] = useState([]);
+  const insertQueen = (x, y) => {
+    const disabledBlock = disabledPositions.length > 0 && disabledPositions.find(a => a.x === x && a.y === y);
+    if (!disabledBlock) {
+      setQueenBlocks(queenBlocks.concat({ x, y }));
+    }
+  };
+  useEffect(() => {
+    const update = findDisabledPos(queens, queenBlocks);
+    if (!isEqual(update, disabledPositions)) setDisabledPositions(update);
+  }, [queenBlocks]);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +30,40 @@ function App() {
         <p>
           Welcome to N-Queens Helper
         </p>
+        <button onClick={() => setQueenBlocks([])}>Reset</button>
       </header>
       <div>
-        
+        {ARR.map(x => (
+          <div key={`div-${x}`}>
+            {ARR.map(y => {
+              const queenPlaced = !!_find(queenBlocks, a => a.x === x && a.y === y);
+              const isDisabled = _find(disabledPositions, a => (a.x === x && a.y === y));
+              return (
+                <Box
+                  key={`pos-${x}-${y}-${isDisabled}`}
+                  x={x}
+                  y={y}
+                  checked={queenPlaced}
+                  onQueenPlace={insertQueen}
+                  disabled={isDisabled}
+                />
+              );
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
+function Box(props) {
+  const { checked, disabled, onQueenPlace, x, y } = props;
+  const onClick = () => {
+    if (!disabled) onQueenPlace(x, y);
+  }
+  return (
+    <div style={getBoxStyle(checked, disabled)} onClick={onClick} key={`${x}-${y}`}>
+    </div>
+  );
+}
 export default App;
+
---
+++ src/utils.js
@@ -0,0 +1,37 @@
+export const getBoxStyle = (checked, disabled) => {
+  const dimension = 40;
+  const background = checked ? 'gold' : disabled ? 'black' : 'white';
+  return {
+    width: `${dimension}px`,
+    height: `${dimension}px`,
+    display: 'inline-block',
+    border: '1px solid',
+    margin: '0',
+    background,
+    boxSizing: 'border-box',
+  };
+};
+
+export function findDisabledPos(maxQueens, queenPos) {
+  const disabled = [];
+  for (let i = 0; i < queenPos.length; i += 1) {
+    const { x, y } = queenPos[i];
+    for (let j = 0; j < maxQueens; j += 1) disabled.push({ x, y: j });
+    for (let j = 0; j < maxQueens; j += 1) disabled.push({ x: j, y });
+    for (let j = 0; j < maxQueens; j += 1) {
+      const t1 = { x: x - j, y: y - j };
+      const t2 = { x: x + j, y: y + j };
+      const t3 = { x: x + j, y: y - j };
+      const t4 = { x: x - j, y: y + j };
+      if (t1.x >= 0 && t1.y >= 0) disabled.push(t1);
+      if (t2.x < maxQueens && t2.y < maxQueens) disabled.push(t2);
+      if (t3.x >= 0 && t3.y >= 0) disabled.push(t3);
+      if (t4.x >= 0 && t4.y >= 0) disabled.push(t4);
+    }
+  }
+  return disabled.reduce((ptOb, pt) => {
+    let tmp = ptOb;
+    if (tmp.indexOf(pt) === -1) tmp = ptOb.concat(pt);
+    return tmp;
+  }, []);
+}
