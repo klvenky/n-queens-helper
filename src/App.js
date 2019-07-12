65d2b890fa50aec606e4d58b2aefeef49ab8792e
klvenky <k.leelavenkatesh@gmail.com>
2019-07-12 16:29:31 +0530

    using service worker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import isEqual from 'lodash/isEqual';
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Box from './box';
-import reactLogo from './react-logo.svg';
-import { Button, LinkButton, Footer } from './silly-comps';
-import Solutions from './solutions';
-import { findDisabledBlocks } from './util-funcs';
+import isEqual from "lodash/isEqual";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ChessBox from "./chess-box";
+import reactLogo from "./react-logo.svg";
+import { Button, LinkButton, Footer } from "./silly-comps";
+import Solutions from "./solutions";
+import { findDisabledBlocks } from "./util-funcs";
 
 const DEFAULT = 8;
 const ARR = [];
 for (let i = 0; i < DEFAULT; i += 1) ARR[i] = i;
 
-function App() {
-  const [queens] = useState(DEFAULT);
+function App(props) {
+  const { queens = 8 } = props;
   const [queenBlocks, setQueenBlocks] = useState([]);
   const [missingSolMsg, setMissingSolMsg] = useState(false);
   const [disabledPositions, setDisabledPositions] = useState([]);
@@ -20,9 +20,11 @@ function App() {
     const update = findDisabledBlocks(queens, queenBlocks);
     const shouldUpdate = !isEqual(update, disabledPositions);
     if (shouldUpdate) setDisabledPositions(update);
-  }, [queenBlocks]);
+  }, [queens, disabledPositions, queenBlocks]);
   const insertQueen = (x, y) => {
-    const disabledBlock = disabledPositions.length > 0 && disabledPositions.find(a => a.x === x && a.y === y);
+    const disabledBlock =
+      disabledPositions.length > 0 &&
+      disabledPositions.find(a => a.x === x && a.y === y);
     if (!disabledBlock) setQueenBlocks(queenBlocks.concat({ x, y }));
   };
   const removeQueen = (x, y) => {
@@ -42,7 +44,7 @@ function App() {
   };
   const resetQueens = () => setQueenBlocks([]);
   const showSolution = () => {
-    const solution = Solutions[queens]
+    const solution = Solutions[queens];
     console.log(solution);
     if (solution) setQueenBlocks(solution);
     else setMissingSolMsg(true);
@@ -52,18 +54,23 @@ function App() {
       <div className="App-header">
         <div>
           <img src={reactLogo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to N-Queens Helper
-          </p>
-          <div><Button onClick={resetQueens}>Reset</Button></div>
-          <div><Button onClick={showSolution}>Solution</Button></div>
+          <p>Welcome to N-Queens Helper</p>
+          <div>
+            <Button onClick={resetQueens}>Reset</Button>
+          </div>
+          <div>
+            <Button onClick={showSolution}>Solution</Button>
+          </div>
           {missingSolMsg && (
             <div>
               <p>Not all solutions are available.</p>
               <p>Please feel free to add your solutions.</p>
-            </div>)}
+            </div>
+          )}
           <div>
-            <LinkButton href="https://github.com/klvenky">+ Follow Me</LinkButton>
+            <LinkButton href="https://github.com/klvenky">
+              + Follow Me
+            </LinkButton>
           </div>
         </div>
       </div>
@@ -72,10 +79,14 @@ function App() {
           {ARR.map(x => (
             <div key={`div-${x}`}>
               {ARR.map(y => {
-                const queenPlaced = !!queenBlocks.find(a => a.x === x && a.y === y);
-                const isDisabled = disabledPositions.find(a => (a.x === x && a.y === y));
+                const queenPlaced = !!queenBlocks.find(
+                  a => a.x === x && a.y === y
+                );
+                const isDisabled = disabledPositions.find(
+                  a => a.x === x && a.y === y
+                );
                 return (
-                  <Box
+                  <ChessBox
                     key={`pos-${x}-${y}-${isDisabled}`}
                     x={x}
                     y={y}
@@ -96,4 +107,3 @@ function App() {
 }
 
 export default App;
-
---
+++ src/chess-box.js
@@ -0,0 +1,23 @@
+import React from "react";
+import crown from "./crown";
+import { getBoxStyle } from "./util-funcs";
+
+export default function ChessBox(props) {
+  const { checked, disabled, insertQueen, x, y, removeQueen } = props;
+  const onClick = () => {
+    if (!disabled) insertQueen(x, y);
+    else if (checked) {
+      console.log("remove queen", { x, y });
+      removeQueen(x, y);
+    }
+  };
+  return (
+    <div
+      style={getBoxStyle(checked, disabled)}
+      onClick={onClick}
+      key={`${x}-${y}`}
+    >
+      {checked && crown()}
+    </div>
+  );
+}
